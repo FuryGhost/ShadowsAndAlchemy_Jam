4e0871748055b84a7e22667cdf8f3699d9d8f0cb
Ari Benditkis <maria.benditkis@gmail.com>
2024-07-20 12:17:27 +0300

    Refactor cursor

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,16 +1,22 @@
 extends Node
 
+const cursor_center: Vector2 = Vector2(32, 32)
+
 @onready var open_hand_cursor: Resource = preload("res://art/cursor/OpenHandCursor.png")
 @onready var closed_hand_cursor: Resource = preload("res://art/cursor/ClosedHandCursor.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_custom_mouse_cursor(open_hand_cursor)
+	_set_cursor(open_hand_cursor)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("click"):
-		Input.set_custom_mouse_cursor(closed_hand_cursor)
-	else:
-		Input.set_custom_mouse_cursor(open_hand_cursor)
+	if Input.is_action_just_pressed("click"):
+		_set_cursor(closed_hand_cursor)
+	elif Input.is_action_just_released("click"):
+		_set_cursor(open_hand_cursor)
+
+
+func _set_cursor(cursor: Resource):
+	Input.set_custom_mouse_cursor(cursor, Input.CURSOR_ARROW, cursor_center)
